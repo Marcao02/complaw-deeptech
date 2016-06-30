6a882c4b25a834f74ceab779e3b39e1c1134b673
meng wong <mengwong@pobox.com>
2016-06-30 23:33:47 +0800

    outputs to english with super crude layout
    also optimizes the top-level clauses to refer to contract as a
    proof-of-concept, so we can evolve and compose the clauses
    
    this isn't going to last, because we can't have multiple clauses that
    each define that the contract is fulfilled.

--- sandbox4/L4v1.hs
+++ sandbox4/L4v1.hs
@@ -4,10 +4,47 @@ module L4v1 where
 
 data Agreement = Agreement { clauses :: [Clause]
                            , effectiveDate :: EffectiveDate
+                           , agreementDate :: Date
                            , parties :: [Party]
                            }
 instance Show Agreement where
-  show a = show $ effectiveDate a
+  show a = (preamble a) ++ agreementDate a ++ "\n"
+    ++ dashify (parties a)
+    ++ "(the \"Parties\")\n\nhereby agree as follows:\n\n"
+    ++ show (effectiveDate a) ++ ",\n\n"
+    ++ joinclauses (map upgradeTopLevel (clauses a))
+
+-- upgrade the top-level clauses to be effective upon contract
+-- rather than effective upon entry into the clause
+upgradeTopLevel :: Clause -> Clause
+upgradeTopLevel = upgradeImmediately . upgradeConsequents
+
+upgradeImmediately :: Clause -> Clause
+upgradeImmediately c
+  | after c == Immediately = c { after = ImmediatelyContract }
+  | otherwise              = c
+
+upgradeConsequents :: Clause -> Clause
+upgradeConsequents c = c { consequent = newconsequent,
+                           reparation = newreparation }
+  where
+    newconsequent = if consequent c == Fulfilled
+                    then FulfilledContract else consequent c
+    newreparation = if reparation c == Breach
+                    then BreachContract    else reparation c
+
+joinclauses :: [Clause] -> String
+joinclauses = concat . map (\c -> show c ++ "\n\n")
+
+dashify :: [Party] -> String
+dashify = concat . map (\p -> "- " ++ p ++ "\n")
+
+preamble :: Agreement -> String
+preamble a
+  | hasConsideration = "This agreement dated "
+  | otherwise        = "Know all men by these presents, that as of "
+    where hasConsideration = any (\clause ->
+                                   isPayment (condition clause)) (clauses a)
 
 data EffectiveDate = UponSignature | UponDate Date
 instance Show EffectiveDate where
@@ -22,10 +59,14 @@ type Party = String
 type FromParty = Party
 type ToParty = Party
 data ToBool = ToBool (Agreement -> Temporal -> Bool)
-
-data After = Immediately | After Date
+instance Eq ToBool where
+  a == b = True
+  
+data After = Immediately | ImmediatelyContract | After Date
+             deriving (Eq)
 instance Show After where
   show Immediately = "Immediately this clause comes into effect"
+  show ImmediatelyContract = "Upon the effective date of this contract"
   show (After date) = "After " ++ date
 
 data Clause = Clause { precondition :: ToBool
@@ -36,11 +77,14 @@ data Clause = Clause { precondition :: ToBool
                      , reparation :: Clause
                      , condition :: Condition
                      }
-            | Fulfilled
-            | Breach
+            | Fulfilled | FulfilledContract
+            | Breach    | BreachContract
+              deriving (Eq)
 instance Show Clause where
   show Fulfilled = "the clause is trivially fulfilled"
   show Breach    = "the clause is breached"
+  show FulfilledContract = "the contract is fulfilled"
+  show BreachContract    = "the contract is breached"
   show Clause { precondition = precondition
               , responsibleParty = responsibleParty
               , after = after
@@ -60,6 +104,7 @@ instance Show Clause where
 
 data Condition = Payment ToParty String Int
                | Gesture String
+                 deriving (Eq)
 
 instance Show Condition where
   show (Payment toParty currency amount) = "pay " ++ toParty ++ " " ++ currency ++ " " ++ show amount
@@ -68,4 +113,8 @@ instance Show Condition where
 describeAction :: Condition -> String
 describeAction (Payment _ _ _) = "making a payment"
 describeAction (Gesture _)     = "making some public gesture"
+
+isPayment :: Condition -> Bool
+isPayment (Payment _ _ _) = True
+isPayment (Gesture _)     = False
 
