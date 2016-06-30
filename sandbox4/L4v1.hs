e1b8bcb98bb088a912a1950d0530547dd6f06a69
meng wong <mengwong@pobox.com>
2016-06-30 22:30:37 +0800

    tied up the Clause.
    next, make this output nicely in an Agreement.

--- sandbox4/L4v1.hs
+++ sandbox4/L4v1.hs
@@ -25,53 +25,47 @@ data ToBool = ToBool (Agreement -> Temporal -> Bool)
 
 data After = Immediately | After Date
 instance Show After where
-  show Immediately = "immediately"
-  show (After date) = "after " ++ date
+  show Immediately = "Immediately this clause comes into effect"
+  show (After date) = "After " ++ date
 
-data Clause = Clause GenericC SpecificC
+data Clause = Clause { precondition :: ToBool
+                     , responsibleParty :: Party
+                     , after :: After
+                     , within :: DateInterval
+                     , consequent :: Clause
+                     , reparation :: Clause
+                     , condition :: Condition
+                     }
             | Fulfilled
             | Breach
 instance Show Clause where
   show Fulfilled = "the clause is trivially fulfilled"
   show Breach    = "the clause is breached"
-  show (Clause
-        ( GenericC { precondition = precondition
-                   , responsibleParty = responsibleParty
-                   , after = after
-                   , within = within
-                   , consequent = consequent
-                   , reparation = reparation
-                   } )
-        specificc)
-    = responsibleParty ++ " is responsible for " ++
-    describeAction specificc ++
-    "; in this instance, " ++ responsibleParty ++ " must " ++
-    show specificc ++
-    "; and must do this " ++ show after ++
-    " within " ++ show within ++ " seconds" ++
-    ". " ++
-    "If this clause is performed, then " ++ show consequent ++
-    ", but if it is not, then " ++ show reparation ++
-    "."
+  show Clause { precondition = precondition
+              , responsibleParty = responsibleParty
+              , after = after
+              , within = within
+              , consequent = consequent
+              , reparation = reparation
+              , condition = condition
+              }
+    = responsibleParty ++ " is responsible for "
+    ++ describeAction condition ++ ". "
+    ++ show after ++ ", "
+    ++ responsibleParty ++ " has " ++ show within ++ " seconds"
+    ++ " to " ++ show condition ++ ". "
+    ++ "If this clause is performed, then " ++ show consequent
+    ++ ", but if it is not, then " ++ show reparation
+    ++ "."
 
-data GenericC = GenericC { precondition :: ToBool
-                         , responsibleParty :: Party
-                         , after :: After
-                         , within :: DateInterval
-                         , consequent :: Clause
-                         , reparation :: Clause
-                         }
-
-data SpecificC = Payment ToParty String Int
+data Condition = Payment ToParty String Int
                | Gesture String
 
-instance Show SpecificC where
-  show (Payment toParty currency amount)
-    = "pay " ++ toParty ++ " " ++ currency ++ show amount
-  show (Gesture g)
-    = "announce " ++ g
+instance Show Condition where
+  show (Payment toParty currency amount) = "pay " ++ toParty ++ " " ++ currency ++ " " ++ show amount
+  show (Gesture g)                       = "announce " ++ g
 
-describeAction :: SpecificC -> String
+describeAction :: Condition -> String
 describeAction (Payment _ _ _) = "making a payment"
-describeAction (Gesture _) = "making some public gesture"
+describeAction (Gesture _)     = "making some public gesture"
 
